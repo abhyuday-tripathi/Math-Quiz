8dc67260c616ae0e4f92e8cc994359a8a1c04401
abhyuday-tripathi <72795925+abhyuday-tripathi@users.noreply.github.com>
2020-12-05 15:09:48 +0530

    Add files via upload

--- gamePage.js
+++ gamePage.js
@@ -1,3 +1,14 @@
+function addUser() {
+  name1 = localStorage.setItem(
+    "player1_name",
+    document.getElementById("userName").value;
+  );
+    name2 = localStorage.setItem(
+    "player2_name",
+    document.getElementById("userName2").value;
+  );
+}
+
 player1_name = localStorage.getItem("player1_name");
 player2_name = localStorage.getItem("player2_name");
 player1_score = 0;
